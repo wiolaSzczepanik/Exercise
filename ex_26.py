3676784bcba627edebab19babcd966e2aadebd9b
mariaBarczyk <barczyk.maria92@gmail.com>
2017-10-30 11:05:51 +0100

    Changes in if block with word ends with e

--- ex_26.py
+++ ex_26.py
@@ -4,16 +4,13 @@ def add_sufix(word):
     consonant = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M',
                  'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Z', 'W', 'Y']
 
-    if word[one_before_last_index:] == 'ee':
-        return word + 'ing'
-    elif word[one_before_last_index:] == 'ie':
-        return word[:-1] + 'ying'
-    elif word[last_letter_index] == 'e':
+    if word[last_letter_index] == 'e':
+        if word[one_before_last_index] == 'e':
+            return word + 'ing'
+        elif word[one_before_last_index] == 'i':
+            return word[:-1] + 'ying'
         return word[:-1] + 'ing'
     elif word[last_letter_index].upper() in consonant:
         return word + word[last_letter_index] + 'ing'
     else:
         return word + 'ing'
-
-
-print(add_sufix('knee'))
