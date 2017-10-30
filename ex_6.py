1da2478631e2bc48ab39884bb897f68de7d402c5
mariaBarczyk <barczyk.maria92@gmail.com>
2017-10-30 10:06:52 +0100

    Add sum and multiply function

---
+++ ex_6.py
@@ -0,0 +1,12 @@
+def sum(list_of_numbers):
+    sum_of_numbers = 0
+    for elem in list_of_numbers:
+        sum_of_numbers += elem
+    return sum_of_numbers
+
+
+def multiply(list_of_numbers):
+    total_multiply = 1
+    for elem in list_of_numbers:
+        total_multiply *= elem
+    return total_multiply
