a1079bc75efc5ca9e52a0e74beb90d01c6b29007
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2017-10-30 09:58:07 +0100

    Final version

--- exe_6_8_11.py
+++ exe_6_8_11.py
@@ -1,6 +1,2 @@
-def _n_chars(number, sign):
-    return number * str(sign)
-
-number = int(input("Enter the number: "))
-sign = str(input("Enter the sign: "))
-print(_n_chars(number, sign))
+def generate_n_chars(number, sign):
+    return number * sign
